f26070671ccc0fcf7d5eb66156728361102b2b21
deepspacetrader <trophytowersgame@gmail.com>
2026-05-25 20:09:35 -0700

    fix base path in vite.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Set base path for GitHub Pages (root of deepspacetrader.github.io)
   server: {
     port: 3000,
     open: true,
